cdb6289049f27edba3cc0dc39bad58d866e7005c
Thiago Elias <thiagoelias99@gmail.com>
2026-03-10 21:13:25 -0300

    build

--- web/src/app/app/(protected)/dashboard/page.tsx
+++ web/src/app/app/(protected)/dashboard/page.tsx
@@ -1,35 +1,9 @@
 "use client"
 
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { SectionCards } from "@/components/section-cards"
-
-import ExampleTable from "@/components/tables/example-table"
-import { DataTablePagination } from "@/components/tables/template/data-table-pagination"
-import { useUsers } from "@/hooks/query/use-users"
-import { parseAsInteger, useQueryStates } from 'nuqs'
-
 export default function DashboardPage() {
-  const [params, setParams] = useQueryStates({
-    page: parseAsInteger.withDefault(1),
-    limit: parseAsInteger.withDefault(8)
-  })
-
-  const { data, isFetching } = useUsers(params)
-
-  const paginatedData = data || {
-    page: 1,
-    limit: 8,
-    total: 0,
-    totalPages: 0,
-    data: [],
-  }
-
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-      <SectionCards />
-      <div className="px-4 lg:px-6">
-        <ChartAreaInteractive />
-      </div>
+
     </div>
   )
 }
