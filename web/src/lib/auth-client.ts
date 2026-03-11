ae0c3ace4748a21391ddd1920f0bf75efc12cee7
Thiago Elias <thiagoelias99@gmail.com>
2026-03-10 23:27:13 -0300

    fck

--- web/src/lib/auth-client.ts
+++ web/src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { admin } from "better-auth/plugins"
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3333",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "https://api.planner.telias.com.br",
   plugins: [
     admin()
   ]
