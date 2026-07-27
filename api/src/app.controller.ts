f77c75701bf4e13f30f0137bbae6672dd70838e7
Thiago Elias <thiagoelias99@gmail.com>
2026-07-27 09:43:36 -0300

    removido criação de repasse entre os meses

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, HttpCode, Post } from '@nestjs/common'
 import { AppService } from './app.service'
-import { ApiTags } from "@nestjs/swagger"
+import { ApiOperation, ApiTags } from "@nestjs/swagger"
 import { AllowAnonymous } from "@thallesp/nestjs-better-auth"
+import { CronService } from "./cron.service"
 
 @ApiTags('App')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService,
+    private readonly cronService: CronService
+  ) { }
 
-  // @Get("sse")
-  // @AllowAnonymous()
-  // getHello(): string {
-  //   return "Hello"
-  // }
+  @AllowAnonymous()
+  @Post("trigger-monthly-transactions")
+  @HttpCode(200)
+  @ApiOperation({ summary: "Manual trigger of monthly transactions" })
+  triggerMonthlyTransactions() {
+    return this.cronService.createMonthlyTransactions()
+  }
 }
